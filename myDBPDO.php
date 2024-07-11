add223c62732d980a1b8f2fad80f50a089020088
Imtiaz Khan <imtiazkhanrobi@gmail.com>
2024-07-11 17:42:07 +0600

    modyfi  commit

--- myDBPDO.php
+++ myDBPDO.php
@@ -1,17 +1,4 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "admit";
-
-try {
-  date_default_timezone_set('Asia/Dhaka');
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-
-    // sql to create table
+le
     $sql = "INSERT INTO `admit`.`admit_cards` (`name`, `father' name`, `mother's name`, `exam center`, `date and time`, `email`) 
     VALUES ($name = htmlspecialchars($_POST['name']);
     $father_name = htmlspecialchars($_POST['father_name']);
